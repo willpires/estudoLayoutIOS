33482f53ed4fd42042d4990f1fc2017486133648
willpires <william.joga91@gmail.com>
2021-10-02 19:06:55 -0300

    subir

---
+++ estudoLayout/estudoLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  estudoLayout
+//
+//  Created by william pires on 02/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        teste()
+        // Do any additional setup after loading the view.
+    }
+    
+    func teste() -> Void {
+        print("aqui")
+    }
+
+
+}
+
